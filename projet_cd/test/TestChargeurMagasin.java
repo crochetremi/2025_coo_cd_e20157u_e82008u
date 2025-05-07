93b31cfe7b584328dcedfd16e5a84fc77f05f873
Rémi Crochet <remicrochet.310@orange.fr>
2025-05-07 09:02:59 +0200

    validation des tests

--- projet_cd/test/TestChargeurMagasin.java
+++ projet_cd/test/TestChargeurMagasin.java
@@ -13,22 +13,38 @@ public class TestChargeurMagasin {
         boolean exeption = false;
 
         try {
-            ChargeurMagasin cm = new ChargeurMagasin("musicbrainzSimple");
+            ChargeurMagasin cm = new ChargeurMagasin("projet_cd/musicbrainzSimple/");
             Magasin m = cm.chargerMagasin();
             nbCds = m.getNombreCds();
         }
         catch(Exception e){
+            System.out.println(e.getMessage());
             exeption = true;
         }
 
         assertFalse(exeption);
-        assertEquals(1, nbCds);
+        assertEquals(12, nbCds);
 
     }
 
-    //@Test
-    //public void repertoireInexistant(){
+    @Test
+    public void repertoireInexistant(){
 
-    //}
+        int nbCds = 0;
+        boolean exeption = false;
+
+        try {
+            ChargeurMagasin cm = new ChargeurMagasin("musicbrainzSimple/");
+            Magasin m = cm.chargerMagasin();
+            nbCds = m.getNombreCds();
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+            exeption = true;
+        }
+
+        assertTrue(exeption);
+
+    }
 
 }
